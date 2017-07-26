2d11f7f2b8f17151f1be66e0e7c111a64698e61d
Chris Dosé <chris.dose@gmail.com>
2017-07-25 21:17:35 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,23 @@
+exports.decorateConfig = config => Object.assign({}, config, {
+  backgroundColor: '#121212',
+  foregroundColor: '#bbbbbb',
+  cursorColor: '#bbbbbb',
+  colors: {
+    black: '#121212',
+    red: '#fa2573',
+    green: '#98e123',
+    yellow: '#dfd460',
+    blue: '#1080d0',
+    magenta: '#8700ff',
+    cyan: '#43a8d0',
+    white: '#bbbbbb',
+    lightBlack: '#555555',
+    lightRed: '#f6669d',
+    lightGreen: '#b1e05f',
+    lightYellow: '#fff26d',
+    lightBlue: '#00afff',
+    lightMagenta: '#af87ff',
+    lightCyan: '#51ceff',
+    lightWhite: '#ffffff'
+  }
+})
